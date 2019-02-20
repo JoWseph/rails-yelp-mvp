474fac1770fa4db5d9980ecbec9e33a68e00c90a
Joseph Wiel <joseph@comet.co>
2019-02-20 11:43:26 +0100

    Prepare rails app with external specs

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,15 @@ puts 'Creating restaurants...'
   restaurant = Restaurant.new
   restaurant.name = Faker::Restaurant.name
   restaurant.address = Faker::Address.full_address
-  restaurant.description = Faker::Restaurant.description
+  restaurant.phone_number = Faker::PhoneNumber.phone_number_with_country_code
   restaurant.category = %w[chinese italian japanese french belgian].sample
-  restaurant.save
-  rand(10..20).times do
-    Review.create(
-      restaurant_id: restaurant,
-      content: Faker::Restaurant.review
-    )
+  restaurant.save!
+  rand(5..15).times do
+    review = Review.new
+    review.restaurant = restaurant
+    review.rating = rand(1..5)
+    review.content = Faker::Restaurant.review
+    review.save!
   end
 end
 puts 'Restaurants created !'
